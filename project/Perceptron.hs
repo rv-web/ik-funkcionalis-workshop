21139cbc0e8fa9dfec8c682d5e88c63819ebd509
Attila Bagossy <bagossyattila@outlook.com>
2018-11-23 01:01:22 +0100

    Introduced type synonyms to increase readability

--- project/Perceptron.hs
+++ project/Perceptron.hs
@@ -1,32 +1,38 @@
+type Label = Float
+type Input = [Float]
+type Example = (Input, Label)
+type Weights = [Float]
+type TrainingSet = [Example]
+
 dot :: [Float] -> [Float] -> Float
 dot a b = sum $ zipWith (*) a b
 
-predict :: [Float] -> [Float] -> Float
-predict sample weights
+predict :: Input -> Weights -> Label
+predict input weights
     | angle < 0 = -1
     | otherwise = 1
     where
-        angle = dot (1 : sample) weights
+        angle = dot (1 : input) weights
 
-adjustWeights :: [Float] -> Float -> [Float] -> [Float]
-adjustWeights sample expected weights
+adjustWeights :: Example -> Weights -> Weights
+adjustWeights (input, expected) weights
     | prediction == expected = weights
     | otherwise = updatedWeights
     where
-        prediction = predict sample weights
+        prediction = predict input weights
         learningRate = 0.1
         mul = learningRate * (-prediction)
-        scaledSample = map (mul *) (1 : sample)
+        scaledSample = map (mul *) (1.0 : input)
         updatedWeights = zipWith (+) weights scaledSample
 
-epoch :: [([Float], Float)] -> [Float] -> ([Float], Float)
+epoch :: TrainingSet -> Weights -> (Weights, Float)
 epoch trainingSet initialWeights = (adjustedWeights, delta)
     where
-        adjustedWeights = foldl (\weights (sample, expected) -> adjustWeights sample expected weights) initialWeights trainingSet
+        adjustedWeights = foldl (\weights example -> adjustWeights example weights) initialWeights trainingSet
         diff = zipWith (-) initialWeights adjustedWeights
         delta = sqrt $ dot diff diff
 
-train :: [([Float], Float)] -> [Float] -> Float -> [Float]
+train :: TrainingSet -> Weights -> Float -> Weights
 train trainingSet initialWeights threshold
     | delta <= threshold = adjustedWeights
     | otherwise = train trainingSet adjustedWeights threshold
