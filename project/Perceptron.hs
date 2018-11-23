cbcefa1678419db326f2f097cf9bb875461734cc
Attila Bagossy <bagossyattila@outlook.com>
2018-11-23 01:34:32 +0100

    Implemented some input checking

--- project/Perceptron.hs
+++ project/Perceptron.hs
@@ -13,6 +13,8 @@ module Perceptron
 
 import qualified Train as T
 
+import Control.Monad (guard)
+
 newtype Perceptron = Perceptron { getWeights :: [Float] } deriving (Show)
 
 matchingDimensions :: T.Input -> T.Weights -> Bool
@@ -27,7 +29,42 @@ predict perceptron input
     where
         weights = getWeights perceptron
 
-fit :: T.TrainingSet -> T.TrainingConfiguration -> Perceptron
-fit trainingSet conf = Perceptron weights
-    where
-        weights = T.fit trainingSet conf
+fit :: T.TrainingSet -> T.TrainingConfiguration -> Maybe Perceptron
+fit trainingSet conf = do
+    guard (length trainingSet > 0)
+    guard (T.learningRate conf > 0)
+    guard (T.threshold conf >= 0)
+    guard (consistentInputLength trainingSet)
+    guard (appropriateBinaryLabels trainingSet)
+    guard (matchingInitialWeightsLength trainingSet (T.initialWeights conf))
+    let weights = T.fit trainingSet conf
+    return $ Perceptron weights
+
+(|>) :: a -> (a -> b) -> b
+x |> f = f x
+
+consistentInputLength :: T.TrainingSet -> Bool
+consistentInputLength trainingSet =
+    let
+        firstInput = fst $ head trainingSet
+        expectedFeatureCount = length firstInput
+    in
+        map fst trainingSet
+        |> map length
+        |> all (expectedFeatureCount ==)
+
+appropriateBinaryLabels :: T.TrainingSet -> Bool
+appropriateBinaryLabels trainingSet = 
+    let
+        isOneOrMinusOne label = abs label == 1
+    in
+        map snd trainingSet 
+        |> all isOneOrMinusOne
+
+matchingInitialWeightsLength :: T.TrainingSet -> T.Weights -> Bool
+matchingInitialWeightsLength trainingSet initialWeights =
+    let
+        firstInput = fst $ head trainingSet
+        expectedWeightCount = length firstInput + 1
+    in
+        expectedWeightCount == length initialWeights
